5837c1173df838eed71fe7e22ca6ca0e2697e059
Sokleab.LAY <laysokleab55@gmail.com>
2026-05-29 12:47:28 +0700

    edit the errot

--- Renting/backend/routes/auth.js
+++ Renting/backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../db');
+const bcrypt = require('bcrypt');
 
 // POST /api/auth/login
 router.post('/login', async (req, res) => {
